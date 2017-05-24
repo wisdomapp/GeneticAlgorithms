55da33060bb1157f7276f254bf9ce82608040f31
j15098so <j15098so@edu.tuis.ac.jp>
2017-05-24 16:44:56 +0900

    ルーレット追加

--- src/GAmain.java
+++ src/GAmain.java
@@ -2,6 +2,7 @@ public class GAmain{
 	public static void main(String[] args){
 		int[] Pop2=Random.Ranview();
 		double[] Pop3=Fitness.Compatible(Pop2);
+		Roulette.Pselect(Pop3);
 		
 	}
 }
---
+++ src/Roulette.java
@@ -0,0 +1,36 @@
+public class Roulette{
+	public static void Pselect(double[] Pop3){
+		double[] PopS=new double[Pop3.length];
+		double tmp=0.0;
+		for(int i=0;i<Pop3.length;i++){
+			tmp +=Pop3[i];
+		}
+		for(int i=0;i<Pop3.length;i++){
+			PopS[i]=Pop3[i]/tmp;
+			System.out.println("Pop2["+i+"]の確率分布は"+PopS[i]);
+		}
+		
+		Select(PopS);
+	}
+	
+	public static void Select(double[] PopS){
+
+		int sle=0;
+		for(int i=0;i<PopS.length;i++){
+			double multi=0.0;
+			double tmp=Math.random();
+			System.out.println(tmp);
+			for(int i2=0;i2<PopS.length;i2++){
+				multi += PopS[i2];
+				if(multi>tmp){
+					sle=i2;
+					break;
+				}
+
+			}
+			System.out.println(sle);
+		}
+	}
+	
+	
+}
