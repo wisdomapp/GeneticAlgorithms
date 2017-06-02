da57e1d9c67edb10960927996641bec75b3c76b0
j15098so <j15098so@edu.tuis.ac.jp>
2017-06-02 12:58:39 +0900

    リスト総数でbreak

--- src/Crossover.java
+++ src/Crossover.java
@@ -12,17 +12,29 @@ public class Crossover{
 		}
 		
 		for(int i=0;i<NewPop.length;i++){
-			while(lst.contains(i)){
-				i++;
+			
+			
+			if(NewPop.length%2==0){
+				if(lst.size()==NewPop.length){
+					System.out.println("break");
+					break;
+				}
+				lst.add(i);
+			}else{
+				lst.add(i);
+				if(lst.size()==NewPop.length){
+					System.out.println("break");
+					break;
+				}
+				
 			}
-			if(i>=NewPop.length){
-				break;
-			}
+			
 			if(Math.random() <= probability){
-				int y=(int) (Math.random()*NewPop[i].length()); //どの集団と交叉させるか
+				int y=(int) (Math.random()*NewPop.length); //どの集団と交叉させるか
 				int m=(int) (Math.random()*NewPop[i].length()); //何文字目で区切るか
-				while(y==i||lst.contains(y)){
-					 y=(int) (Math.random()*NewPop[i].length());
+				while(lst.contains(y)){
+					 y=(int) (Math.random()*NewPop.length);
+					 System.out.println("roop");
 				}
 				System.out.println("NewPoP["+i+"]はNewPop["+y+"]と"+(m+1)+"文字目で交叉");
 				String tmp1=tmp[i].substring(0,m);
@@ -31,7 +43,7 @@ public class Crossover{
 				String tmp4=tmp[y].substring(m);
 				tmp[i]=tmp1+tmp4;
 				tmp[y]=tmp3+tmp2;
-				lst.add(i);
+				//lst.add(i);
 				lst.add(y);
 				
 			}
